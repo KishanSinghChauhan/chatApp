b23f05f1187dcbc45cbf6296ee251687fe653dcc
KishanSinghChauhan <kishansingh705446@gmail.com>
2020-12-13 14:14:51 +0530

    add route and components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import React from 'react';
+import {BrowserRouter as Router, Route } from 'react-router-dom';
+import Chat from './Components/Chat';
+import Join from './Components/Join';
+
+
 function App() {
   return (
-    <div>
-      <h1>hello</h1>
-    </div>
+    <Router>
+      <Route path='/' exact component={Join}/>
+      <Route path='/chat' exact component={Chat}/>
+    </Router>
   );
 }
 
